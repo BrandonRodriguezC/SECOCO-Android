7250419bbe21fb018b792b80c01567c4af889231
brandonrodriguez <312511Ba>
2020-12-23 14:49:15 -0500

    Adicion de DatePicker

--- app/src/main/java/com/example/secoco/Registro.java
+++ app/src/main/java/com/example/secoco/Registro.java
@@ -1,14 +1,13 @@
 package com.example.secoco;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.DatePicker;
-import android.app.DatePickerDialog;
 
-import java.text.SimpleDateFormat;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 public class Registro extends AppCompatActivity {
 
@@ -21,14 +20,24 @@ public class Registro extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
         getSupportActionBar().setTitle("Registro");
 
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("Seleccione su fecha de nacimiento");
+        final MaterialDatePicker materialDatePicker= builder.build();
         this.txt_fecha_nacimiento = (EditText) findViewById(R.id.txt_fecha_nacimiento);
 
         this.txt_fecha_nacimiento.setOnClickListener(new View.OnClickListener() {@Override
-        public void onClick(View view) {seleccionarFecha(); }});
+        public void onClick(View view) {
+            materialDatePicker.show(getSupportFragmentManager(), "Date_Picker ");
+
+        }});
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                txt_fecha_nacimiento.setText(materialDatePicker.getHeaderText());
+            }
+        });
     }
 
-    public void seleccionarFecha() {
-
-    }
 
 }
