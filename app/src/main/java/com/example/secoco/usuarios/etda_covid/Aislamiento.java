cec6e533fc6b3b16e3ac19415f64fbed068bf86d
Diego Adrián Meneses Romero <diegoadriano@hotmail.com>
2021-01-22 20:20:12 -0500

    Reporte Movimiento Zona (Hecho por Cristián, limpiado por Diego)

--- app/src/main/java/com/example/secoco/usuarios/etda_covid/Aislamiento.java
+++ app/src/main/java/com/example/secoco/usuarios/etda_covid/Aislamiento.java
@@ -1,9 +1,9 @@
 package com.example.secoco.usuarios.etda_covid;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,12 +16,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.secoco.R;
 import com.example.secoco.general.RequestAPI;
-import com.example.secoco.usuarios.erc_covid.ReporteNotificarCita;
-import com.example.secoco.usuarios.erc_covid.recyclerView.AdaptadorRecyclerNotificarCita;
-import com.example.secoco.usuarios.erc_covid.recyclerView.Item;
 import com.example.secoco.usuarios.etda_covid.recyclerViewAis.AdapterAis;
 import com.example.secoco.usuarios.etda_covid.recyclerViewAis.ItemAis;
-import com.google.gson.JsonObject;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -36,7 +32,6 @@ public class Aislamiento extends AppCompatActivity implements View.OnClickListen
     private ArrayList<ItemAis> itemAis;
     private Button btnEnviar;
 
-    private TextView usr_ais, nombre_ais, correo_ais, cedula_ais, fecha_ais;
     private JSONObject localidad;
     private Spinner spinner;
     private String locali;
@@ -46,7 +41,6 @@ public class Aislamiento extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         getSupportActionBar().setTitle("Reporte aislamiento");
         setContentView(R.layout.activity_aislamiento);
-
 
         itemAis = new ArrayList<>();
 
@@ -68,18 +62,11 @@ public class Aislamiento extends AppCompatActivity implements View.OnClickListen
     @Override
     public void onClick(View view) {
         if (view.getId() == btnEnviar.getId()) {
-            nombre_ais = (TextView) findViewById(R.id.item_nombre_ais);
-            correo_ais = (TextView) findViewById(R.id.item_correo_ais);
-            cedula_ais = (TextView) findViewById(R.id.item_cedula_ais);
-            usr_ais = (TextView) findViewById(R.id.item_usuario_ais);
-            fecha_ais=(TextView) findViewById(R.id.item_fecha_ais);
-
             spinner = (Spinner) findViewById(R.id.spiner_localidad);
-            locali = spinner.getSelectedItem().toString();
             JSONObject request = new JSONObject();
-
+            String locali_numero = getResources().getStringArray(R.array.LocalidadesIdentificador)[spinner.getSelectedItemPosition()];
             try {
-                request.put("Z", spinner);
+                request.put("Z", locali_numero);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -90,8 +77,7 @@ public class Aislamiento extends AppCompatActivity implements View.OnClickListen
                         @Override
                         public void onResponse(JSONObject response) {
                             try {
-                                Toast.makeText(view.getContext(), "La zona se ha filtrado"
-                                        , Toast.LENGTH_SHORT).show();
+                                Toast.makeText(view.getContext(), "La zona se ha filtrado", Toast.LENGTH_SHORT).show();
 
                                 JSONArray z = response.getJSONArray("Z");
                                 for (int i = 0; i < z.length(); i++) {
@@ -99,26 +85,19 @@ public class Aislamiento extends AppCompatActivity implements View.OnClickListen
                                 }
                                 adaptador.notifyDataSetChanged();
 
-
                             } catch (JSONException e) {
                                 e.printStackTrace();
-
                             }
                         }
                     },
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(view.getContext(), "Error de Conexión"
-                                    , Toast.LENGTH_SHORT).show();
+                            Toast.makeText(view.getContext(), "Error de Conexión", Toast.LENGTH_SHORT).show();
                         }
                     });
             jsonObjectRequest.setShouldCache(false);
             RequestAPI.getInstance(view.getContext()).add(jsonObjectRequest);
-
-
-
-
         }
     }
 }
--- app/src/main/java/com/example/secoco/usuarios/etda_covid/recyclerViewAis/AdapterAis.java
+++ app/src/main/java/com/example/secoco/usuarios/etda_covid/recyclerViewAis/AdapterAis.java
@@ -1,21 +1,21 @@
 package com.example.secoco.usuarios.etda_covid.recyclerViewAis;
-import android.util.JsonReader;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.secoco.R;
+
 import java.util.ArrayList;
-import com.example.secoco.usuarios.etda_covid.Aislamiento;
-import com.google.gson.JsonObject;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class AdapterAis extends RecyclerView.Adapter<AdapterAis.ViewHolderDatos> {
+public class AdapterAis extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
 
     ArrayList<ItemAis> datos;
@@ -24,35 +24,37 @@ public class AdapterAis extends RecyclerView.Adapter<AdapterAis.ViewHolderDatos>
     public AdapterAis(ArrayList<ItemAis> datos) {
         this.datos = datos;
     }
-    //Esta bien
-    @NonNull @Override public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).
                 inflate(R.layout.item_aislamiento, parent, false);
         return new ViewHolderDatos(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         ViewHolderDatos asignarDatos = (ViewHolderDatos) holder;
         asignarDatos.setLocali(datos.get(position).getLocalidad());
-
-
     }
+
     //Esta bien
-    @Override public int getItemCount() {
+    @Override
+    public int getItemCount() {
         return datos.size();
     }
 
     public class ViewHolderDatos extends RecyclerView.ViewHolder {
         private TextView usr_ais, nombre_ais, correo_ais, cedula_ais, fecha_ais;
         private JSONObject localidad;
+
         public ViewHolderDatos(@NonNull View itemView) {
             super(itemView);
             this.nombre_ais = (TextView) itemView.findViewById(R.id.item_nombre_ais);
             this.correo_ais = (TextView) itemView.findViewById(R.id.item_correo_ais);
             this.cedula_ais = (TextView) itemView.findViewById(R.id.item_cedula_ais);
             this.usr_ais = (TextView) itemView.findViewById(R.id.item_usuario_ais);
-            this. fecha_ais = (TextView) itemView.findViewById(R.id.item_fecha_ais);
+            this.fecha_ais = (TextView) itemView.findViewById(R.id.item_fecha_ais);
 
         }
 
@@ -65,16 +67,12 @@ public class AdapterAis extends RecyclerView.Adapter<AdapterAis.ViewHolderDatos>
                 this.usr_ais.setText(localidad.getString("usuario"));
                 this.fecha_ais.setText(localidad.getString("fecha"));
 
-            }
-
-            catch (JSONException e) {
+            } catch (JSONException e) {
                 System.out.println(e.getMessage());
                 e.printStackTrace();
             }
         }
     }
-
-
 
 }
 
